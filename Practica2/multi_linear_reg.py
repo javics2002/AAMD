d2199fec9fb10c9105352be2054462f70d4decff
javics2002 <49459590+javics2002@users.noreply.github.com>
2023-10-19 17:39:54 +0200

    Practica 2 empezada
    Co-Authored-By: josemiguelvdz <josemiguelvdz@gmail.com>

---
+++ Practica2/multi_linear_reg.py
@@ -0,0 +1,105 @@
+import numpy as np
+import copy
+import math
+
+
+def zscore_normalize_features(X):
+    """
+    computes  X, zcore normalized by column
+
+    Args:
+      X (ndarray (m,n))     : input data, m examples, n features
+
+    Returns:
+      X_norm (ndarray (m,n)): input normalized by column
+      mu (ndarray (n,))     : mean of each feature
+      sigma (ndarray (n,))  : standard deviation of each feature
+    """
+    
+    mu = np.mean(X, axis = 0)
+    sigma = np.std(X, axis = 0)
+
+    X_norm = (X - mu) / sigma
+    
+    return (X_norm, mu, sigma)
+
+
+def compute_cost(X, y, w, b):
+    """
+    compute cost
+    Args:
+      X (ndarray (m,n)): Data, m examples with n features
+      y (ndarray (m,)) : target values
+      w (ndarray (n,)) : model parameters  
+      b (scalar)       : model parameter
+    Returns
+      cost (scalar)    : cost
+    """
+    n = len(y)
+    cost = 0
+
+    predictions = np.dot(X, w) + b
+    squared_errors = (predictions - y) ** 2
+    cost = (1 / (2 * n)) * np.sum(squared_errors)
+    return cost
+
+
+def compute_gradient(X, y, w, b):
+    """
+    Computes the gradient for linear regression 
+    Args:
+      X : (ndarray Shape (m,n)) matrix of examples 
+      y : (ndarray Shape (m,))  target value of each example
+      w : (ndarray Shape (n,))  parameters of the model      
+      b : (scalar)              parameter of the model      
+    Returns
+      dj_db : (scalar)             The gradient of the cost w.r.t. the parameter b. 
+      dj_dw : (ndarray Shape (n,)) The gradient of the cost w.r.t. the parameters w. 
+    """
+    
+    n = len(y)
+
+    predictions = np.dot(X, w) + b
+    error = predictions - y
+    
+    dj_dw = (1 / n) * np.dot(X.T, error)
+    dj_db = (1 / n) * np.sum(error)
+
+    return dj_db, dj_dw
+
+
+def gradient_descent(X, y, w_in, b_in, cost_function,
+                     gradient_function, alpha, num_iters):
+    """
+    Performs batch gradient descent to learn theta. Updates theta by taking 
+    num_iters gradient steps with learning rate alpha
+
+    Args:
+      X : (array_like Shape (m,n)    matrix of examples 
+      y : (array_like Shape (m,))    target value of each example
+      w_in : (array_like Shape (n,)) Initial values of parameters of the model
+      b_in : (scalar)                Initial value of parameter of the model
+      cost_function: function to compute cost
+      gradient_function: function to compute the gradient
+      alpha : (float) Learning rate
+      num_iters : (int) number of iterations to run gradient descent
+    Returns
+      w : (array_like Shape (n,)) Updated values of parameters of the model
+          after running gradient descent
+      b : (scalar)                Updated value of parameter of the model 
+          after running gradient descent
+      J_history : (ndarray): Shape (num_iters,) J at each iteration,
+          primarily for graphing later
+    """
+    
+    w, b = w_in, b_in
+    J_history = np.zeros(num_iters)
+    
+    for i in range(num_iters):
+        J_history[i] = cost_function(X.T, y, w, b)
+        grad_b, grad_w = gradient_function(X, y, w, b)
+        
+        w -= alpha * grad_w
+        b -= alpha * grad_b
+
+    return w, b, J_history
