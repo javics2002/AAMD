2cce145220d0419ff4d1cd1aa00c0f8774d75cfa
javics2002 <49459590+javics2002@users.noreply.github.com>
2023-12-03 16:19:08 +0100

    Ejercicio 1 general

--- Practica5/ann.py
+++ Practica5/ann.py
@@ -1,81 +1,72 @@
 import numpy as np
 from utils import sig
 
-def feedForward(theta1, theta2, a1):
+def feedForward(theta_list, a_in):
     """
     Perform feedforward propagation in a neural network.
 
     Parameters
     ----------
-    theta1 : array_like
-        Weights for the first layer in the neural network.
-        It has shape (2nd hidden layer size x input size).
-    theta2: array_like
-        Weights for the second layer in the neural network.
-        It has shape (output layer size x 2nd hidden layer size).
-    a1 : array_like
-        Input data with shape (number of examples x input size).
+    theta_list : list of array_like
+        List of weight matrices for each layer in the neural network.
+        Each matrix has shape (number of units in current layer x number of units in previous layer + 1).
+    a_in : array_like
+        Input data with shape (number of examples x number of features).
 
     Returns
     -------
-    a1 : array_like
-        Input data with bias term added.
-    a2 : array_like
-        Activations in the second layer.
-    a3 : array_like
-        Activations in the output layer.
+    activations : list of array_like
+        List containing activations for each layer in the neural network.
     """
     
-    m = a1.shape[0]
-    a1s = np.hstack([np.ones((m, 1)), a1])
+    m = a_in.shape[0]
+    a_in = np.hstack([np.ones((m, 1)), a_in])
+    activations = [a_in]
     
-    a1 = np.column_stack((np.ones((a1.shape[0], 1)), a1))
-    
-    a2 = sig(np.dot(a1s, theta1.T))
-    
-    a2 = np.hstack([np.ones((m, 1)), a2])
-    
-    a3 = sig(np.dot(a2, theta2.T))
-    print(a3.shape)
-    
-    return a1, a2, a3
+    for i in range(len(theta_list)):
+        z = np.dot(activations[-1], theta_list[i].T)
+        a = sig(z)
+        
+        if (i < len(theta_list) - 1):
+            a = np.hstack([np.ones((m, 1)), a])
+            
+        activations.append(a)
+        
+    return activations
 
 
-def predict(theta1, theta2, a1):
+def predict(theta_list, a_in):
     """
     Predict the label of an input given a trained neural network.
-    
+
     Parameters
     ----------
-    theta1 : array_like
-        Weights for the first layer in the neural network.
-        It has shape (2nd hidden layer size x input size)
-    theta2: array_like
-        Weights for the second layer in the neural network.
-        It has shape (output layer size x 2nd hidden layer size)
-    a1 : array_like
-        The image inputs having shape (number of examples x image dimensions).
-    
-    Return
-    ------
+    theta_list : list of array_like
+        List of weight matrices for each layer in the neural network.
+        Each matrix has shape (number of units in current layer x number of units in previous layer + 1).
+    a_in : array_like
+        Input data with shape (number of examples x number of features).
+
+    Returns
+    -------
     p : array_like
         Predictions vector containing the predicted label for each example.
         It has a length equal to the number of examples.
     """
     
-    a1, a2, a3 = feedForward(theta1, theta2, a1)
+    activations = feedForward(theta_list, a_in)
     
-    p = np.argmax(a3, axis=1)
+    p = np.argmax(activations[-1], axis=1)
 
     return p
 
-def predict_without_feedforward(a3):
+def predict_without_feedforward(out_a):
     """
     Predict the label given the activations in the output layer.
 
     Parameters
     ----------
-    a3 : array_like
+    out_a : array_like
         Activations in the output layer.
 
     Returns
@@ -85,72 +76,56 @@ def predict_without_feedforward(a3):
         It has a length equal to the number of examples.
     """
     
-    p = np.argmax(a3, axis=1)
+    p = np.argmax(out_a, axis=1)
 
     return p
 
-def cost(theta1, theta2, X, y, lambda_):
+def cost(theta_list, X, y, lambda_):
     """
-    Compute cost for 2-layer neural network. 
+    Compute the cost for a neural network.
 
     Parameters
     ----------
-    theta1 : array_like
-        Weights for the first layer in the neural network.
-        It has shape (2nd hidden layer size x input size + 1)
-
-    theta2: array_like
-        Weights for the second layer in the neural network. 
-        It has shape (output layer size x 2nd hidden layer size + 1)
-
+    theta_list : list of array_like
+        List of weight matrices for each layer in the neural network.
+        Each matrix has shape (number of units in current layer x number of units in previous layer + 1).
     X : array_like
-        The inputs having shape (number of examples x number of dimensions).
-
+        Input data with shape (number of examples x number of features).
     y : array_like
         1-hot encoding of labels for the input, having shape 
-        (number of examples x number of labels).
-
+        (number of examples x number of classes).
     lambda_ : float
-        The regularization parameter. 
+        Regularization parameter.
 
     Returns
     -------
     J : float
-        The computed value for the cost function. 
-
+        The computed value for the cost function.
     """
-
-    m = len(y)
     
-    a1, a2, a3 = feedForward(theta1, theta2, X)
-    predictions = a3
+    m = len(y)
+    activations = feedForward(theta_list, X)
+    predictions = activations[-1]
     
     J = (-1 / m) * np.sum(y * np.log(predictions) + (1 - y) * np.log(1 - predictions))
     
     reg_term = 0
-    
-    reg_term += np.sum(theta1[:, 1:] ** 2)
-    reg_term += np.sum(theta2[:, 1:] ** 2)
-        
+    for theta in theta_list:
+        reg_term += np.sum(theta[:, 1:]**2)
     reg_term *= (lambda_ / (2 * m))
-    
     J += reg_term
     
     return J
 
-def backprop(theta1, theta2, X, y, lambda_):
+def backprop(theta_list, X, y, lambda_):
     """
     Compute cost and gradient for 2-layer neural network. 
 
     Parameters
     ----------
-    theta1 : array_like
-        Weights for the first layer in the neural network.
-        It has shape (2nd hidden layer size x input size + 1)
-
-    theta2: array_like
-        Weights for the second layer in the neural network. 
-        It has shape (output layer size x 2nd hidden layer size + 1)
+    theta_list : list of array_like
+        List of weight matrices for each layer in the neural network.
+        Each matrix has shape (number of units in current layer x number of units in previous layer + 1).
 
     X : array_like
         The inputs having shape (number of examples x number of dimensions).
@@ -182,28 +157,21 @@ def backprop(theta1, theta2, X, y, lambda_):
     m = X.shape[0]
     n_labels = theta_list[-1].shape[0]
 
-    # Forward propagation
     activations, zs = forward_propagation(X, theta_list)
     a_last = activations[-1]
 
-    # Cost calculation
     J = cost(theta_list, X, y, lambda_)
 
-
-    # Backpropagation
     deltas = [a_last - y]
     for i in range(len(theta_list) - 1, 0, -1):
         delta = np.dot(deltas[0], theta_list[i][:, 1:])*sigmoid_gradient(zs[i - 1])
         deltas.insert(0, delta)
 
-    # Gradients
     grads = [np.dot(d.T, a) / m for d, a in zip(deltas, activations[:-1])]
 
-    # Regularization term gradients (excluding bias terms)
     for i in range(len(grads)):
         grads[i][:, 1:] += (lambda_ / m) * theta_list[i][:, 1:]
 
-    # Rename gradients
     grad1, grad2 = grads[0], grads[1]
 
     return J, grad1, grad2
